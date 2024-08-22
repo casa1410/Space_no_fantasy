cbe44580c007889d9affe4689a510cc5758af2a3
Sebastian Casadiego <sacasadiego@gmail.com>
2024-08-21 21:34:05 -0500

    First commit

---
+++ src/main/java/com/code/space_no_fantasy/Space_no_fantasy.java
@@ -0,0 +1,8 @@
+package com.code.space_no_fantasy;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Space_no_fantasy.MODID)
+public class Space_no_fantasy {
+    public static final String MODID = "space_no_fantasy";
+}
